c341edf3f913b9ff191d422f3e17217ae4040ccd
Titus Low <89781573+titim789@users.noreply.github.com>
2023-04-20 23:51:59 +0800

    Update UsersChoice.js

--- controllers/UsersChoice.js
+++ controllers/UsersChoice.js
@@ -64,7 +64,11 @@ export const getUserTopFoodChoice = async (req, res) => {
         userId: user[0].id,
       },
     });
-
+    console.log(usertopfoodchoice);
+    const resultString = JSON.stringify(usertopfoodchoice, null, 2);
+    if (resultString === "[]") {
+      return res.status(200).json({ usertopfoodchoice: [] });
+    }
     var concat_usertopfoodchoice = [];
     Object.keys(usertopfoodchoice[0]).forEach((rank) => {
       const foodChoice = usertopfoodchoice[0][rank];
@@ -94,36 +98,36 @@ export const setUserTopFoodChoice = async (req, res) => {
     });
 
     const payload = {
-      rank1: req.body.rank1,
-      rank2: req.body.rank2,
-      rank3: req.body.rank3,
-      rank4: req.body.rank4,
-      rank5: req.body.rank5,
-      rank6: req.body.rank6,
-      rank7: req.body.rank7,
-      rank8: req.body.rank8,
-      rank9: req.body.rank9,
-      rank10: req.body.rank10,
-      rank11: req.body.rank11,
-      rank12: req.body.rank12,
-      rank13: req.body.rank13,
-      rank14: req.body.rank14,
-      rank15: req.body.rank15,
-      rank16: req.body.rank16,
-      rank17: req.body.rank17,
-      rank18: req.body.rank18,
-      rank19: req.body.rank19,
-      rank20: req.body.rank20,
-      rank21: req.body.rank21,
-      rank22: req.body.rank22,
-      rank23: req.body.rank23,
-      rank24: req.body.rank24,
-      rank25: req.body.rank25,
-      rank26: req.body.rank26,
-      rank27: req.body.rank27,
-      rank28: req.body.rank28,
-      rank29: req.body.rank29,
-      rank30: req.body.rank30,
+      rank1: req.body[0],
+      rank2: req.body[1],
+      rank3: req.body[2],
+      rank4: req.body[3],
+      rank5: req.body[4],
+      rank6: req.body[5],
+      rank7: req.body[6],
+      rank8: req.body[7],
+      rank9: req.body[8],
+      rank10: req.body[9],
+      rank11: req.body[10],
+      rank12: req.body[11],
+      rank13: req.body[12],
+      rank14: req.body[13],
+      rank15: req.body[14],
+      rank16: req.body[15],
+      rank17: req.body[16],
+      rank18: req.body[17],
+      rank19: req.body[18],
+      rank20: req.body[19],
+      rank21: req.body[20],
+      rank22: req.body[21],
+      rank23: req.body[22],
+      rank24: req.body[23],
+      rank25: req.body[24],
+      rank26: req.body[25],
+      rank27: req.body[26],
+      rank28: req.body[27],
+      rank29: req.body[28],
+      rank30: req.body[29],
       userId: user[0].id,
     };
 
